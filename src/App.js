9695aff534214d35d9970a8f0e83be4d89bfa42c
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-05-09 19:55:32 +0300

    feat: Add link for dynamic routes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import HomePage from "./pages/Home.js";
 import ProductsPage from "./pages/Products.js";
 import RootLayout from "./pages/Root.js";
 import ErrorPage from "./pages/Error.js";
+import ProductDetailPage from "./pages/ProductDetail.js";
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -24,6 +25,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
+      { path: "/products/:productId", element: <ProductDetailPage /> },
     ],
   },
 ]);
