50410506a3467a70f110e24597fb719ebc20598c
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-05-09 23:19:34 +0300

    chore: Clear comments

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import {
-  createBrowserRouter,
-  // createRoutesFromElements,
-  RouterProvider,
-  // Route,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home.js";
 import ProductsPage from "./pages/Products.js";
 import RootLayout from "./pages/Root.js";
 import ErrorPage from "./pages/Error.js";
 import ProductDetailPage from "./pages/ProductDetail.js";
-
-// const routeDefinitions = createRoutesFromElements(
-//   <Route>
-//     <Route path="/" element={<HomePage />} />
-//     <Route path="/products" element={<ProductsPage />} />
-//   </Route>
-// );
 
 const router = createBrowserRouter([
   {
@@ -29,8 +17,6 @@ const router = createBrowserRouter([
     ],
   },
 ]);
-
-// const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
